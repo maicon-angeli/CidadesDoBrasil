2874b143166714e60f42e3c19201cfb4e6be17e5
Maicon Angeli <maicon_angeli@hotmail.com>
2022-03-23 14:18:39 -0300

    flywatBD

--- src/test/java/com/citiesapi/service/DistanceServiceTest.java
+++ src/test/java/com/citiesapi/service/DistanceServiceTest.java
@@ -3,18 +3,23 @@ package com.citiesapi.service;
 import com.citiesapi.entities.City;
 import com.citiesapi.repository.CityRepository;
 import com.citiesapi.utils.EarthRadius;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.geo.Point;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.offset;
-import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
@@ -35,6 +40,8 @@ public class DistanceServiceTest {
         saoCarlos =
                 new City(5254L, "São Carlos", 26, 3548906, "(-22.01740074157715,-47.88600158691406)",
                         new Point(-22.01740074157715, -47.88600158691406));
+        Logger log = LoggerFactory.getLogger(DistanceService.class);
+
     }
 
     @Test
@@ -72,4 +79,14 @@ public class DistanceServiceTest {
 
         assertThat(distance).isEqualTo(12426.810463475855);
     }
+    @Test
+    public void shouldCalculateInMilesUsingPoints() {
+
+        given(cityRepository.findAllById(anyList())).willReturn(Arrays.asList(ibate, saoCarlos));
+
+        Double distance = service.distanceByPointsInMiles(4929L,5254L);
+
+        assertThat(distance).isEqualTo(12426.810463475855);
+    }
+
 }
